7bee19368894c93551d1f60cf38a0fb74a538d14
Roberto Luiz Souza Monteiro <roberto@souzamonteiro.com>
2020-07-16 18:59:44 -0300

    First commit

---
+++ js/System.js
@@ -0,0 +1,91 @@
+/**
+ * @license
+ * Copyright 2020 Roberto Luiz Souza Monteiro,
+ *                Renata Souza Barreto,
+ *                Hernane Borges de Barros Pereira.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ /** MaiaScript system library. */
+ function System() {
+    init();
+
+    /**
+     * Creates the attributes of the class.
+     */
+    function init() {
+        // Class attributes goes here.
+    }
+
+    /**
+     * Displays a message in a dialog box.
+     * @param {string}  text - Text to display.
+     */
+    this.showMessageDialog = function(text)
+    {
+        alert(text);
+    }
+
+    /**
+     * Displays a message in a dialog box asking you to enter text.
+     * @param {string}   text - Text to display.
+     * @param {string}   defaultText - Default text to display in the text box.
+     * @return {string}  User-typed text.
+     */
+    this.showInputDialog = function(text, defaultText = '')
+    {
+        return prompt(text, defaultText);
+    }
+
+    /**
+     * Displays a message in a dialog box asking for confirmation.
+     * @param {string}   text - Text to display.
+     * @return {string}  User choice.
+     */
+    const showConfirmDialog = function(text)
+    {
+        return confirm(text);
+    }
+
+    /**
+     * Displays a message in the console.
+     * @param {string}  text - Text to display.
+     */
+    this.print = function(text)
+    {
+        console.log(text);
+    }
+
+   /**
+     * Displays a formated string based on format specifiers passed to the function.
+     * @param {string}      fmt - A string containing format specifiers.
+     * @param {object ...}  arguments - Objects to be formatted.
+     * @return {string}     A formatted string based on format specifiers passed to the function.
+     */
+    this.printf = function(fmt)
+    {
+        console.log(string.sprintFormat(string.sprintfParse(fmt), arguments));
+    }
+
+    /**
+     * Displays a message on the console and advances the cursor to the next line.
+     * @param {string}  text - Text to display.
+     */
+    this.println = function(text)
+    {
+        console.log(text + '\n');
+    }
+}
+
+system = new System();
