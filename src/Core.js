f0ddb942aa4d43a4dd0b946c276a6d0f990c6d52
Roberto Luiz Souza Monteiro <roberto@souzamonteiro.com>
2021-01-13 15:13:53 -0200

    Fix on version number

--- src/Core.js
+++ src/Core.js
@@ -26,7 +26,7 @@ function Core() {
      * This property needs to be updated
      * with each new version of MaiaStudio.
      */
-    this.version = "3.1.2";
+    this.version = "3.1.3";
 
     this.testResult = {
         "expected": {},
