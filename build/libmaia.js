8c5e02f28b439898851d149ced0c5461e203a546
Roberto Luiz Souza Monteiro <roberto@souzamonteiro.com>
2021-03-05 14:36:20 -0300

    Updated version number

--- build/libmaia.js
+++ build/libmaia.js
@@ -5967,7 +5967,7 @@ function Core() {
      * This property needs to be updated
      * with each new version of MaiaStudio.
      */
-    this.version = "3.2.5";
+    this.version = "3.2.6";
 
     this.testResult = {
         "expected": {},
