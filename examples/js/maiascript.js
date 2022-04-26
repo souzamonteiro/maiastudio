cfe85a640dfb1bc3c1a1899becf4da884325b2b7
souzamonteiro <roberto@souzamonteiro.com>
2022-04-26 13:01:23 +0000

    Removed some dependencies

--- examples/js/maiascript.js
+++ examples/js/maiascript.js
@@ -6214,7 +6214,7 @@ function Core() {
      * This property needs to be updated
      * with each new version of MaiaStudio.
      */
-    this.version = "3.7.2";
+    this.version = "3.8.1";
 
     this.testResult = {
         "expected": {},
